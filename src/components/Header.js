e2ef919c8b9f513f54d291e15f9dcb56a330ea8b
jpetrasek1217 <josephpetrasek1017@gmail.com>
2023-08-12 01:04:48 -0400

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,9 +20,9 @@ function Header(){
 
     return(
         <div className={classes.headerContainer}>
-            <Link className={classes.Link} to="/" ><img src={melLogo} alt='McMaster Extra Life Logo' className={classes.headerItem} /></Link>
+            <Link className={classes.Link} to="/MelPersonalityQuiz" ><img src={melLogo} alt='McMaster Extra Life Logo' className={classes.headerItem} /></Link>
             <h2 className={classes.headerText}>Which McMaster Extra Life Exec Are You?</h2>
-            <Link className={classes.Link} to="/quiz" onClick={handleClick}><img src={restart} alt='Restart Quiz' className={classes.headerItem}  /></Link>
+            <Link className={classes.Link} to="/MelPersonalityQuiz" onClick={handleClick}><img src={restart} alt='Restart Quiz' className={classes.headerItem}  /></Link>
         </div>
     );
 }
